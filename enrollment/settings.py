d2424c8aba8a003c5d9ee7c8dc7ee2faf4615489
Natchanon Chanrungrojne <6410615048@student.tu.ac.th>
2023-10-22 19:55:49 +0700

    fix issues

--- enrollment/settings.py
+++ enrollment/settings.py
@@ -124,11 +124,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://hostmane'
+    'https://hostname'
 ]
-
-# Add this code at the end
-try:
-    from .local_settings import *
-except ImportError:
-    pass
